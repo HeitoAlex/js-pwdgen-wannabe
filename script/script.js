5ef3b960aa821070c2d9c58550ccf51a0462d917
HeitoAlex <giallorossotuttalavita@hotmail.it>
2024-05-07 16:19:26 +0200

    esercizio js completo

---
+++ script/script.js
@@ -0,0 +1,5 @@
+let userName = prompt('Il tuo nome?')
+let userSurname = prompt('Il tuo cognome?')
+let userColor = prompt('Il tuo colore preferito?')
+
+document.getElementById("output").innerHTML = (userName + userSurname + userColor + 24)
